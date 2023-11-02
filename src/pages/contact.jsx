71b4b3775d53c6ea9e2ef9c49e618527e69f0d46
Navaneeth-R-Krishnan <91682608+Navaneeth-R-Krishnan@users.noreply.github.com>
2023-11-02 12:53:58 +0530

    Add files via upload

---
+++ src/pages/contact.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Contact = () => {
+
+  return ( <div>
+    
+    <div className="warning">For furthur queries, suggestions and complaints, feel free to contact us!</div>
+    <div> Contact - Bangalore, Karnataka, India. 
+      </div>
+      <div>Email: abovetech@198.company.com</div>
+      </div>);
+};
